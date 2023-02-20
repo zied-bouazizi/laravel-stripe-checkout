aa3f765efb7767420e1c6e5400dc44205009b724
Zied Bouazizi <zied.bouazizi@outlook.com>
2023-02-20 13:16:57 +0100

    Added webhook

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -82,4 +82,47 @@ class ProductController extends Controller
     public function cancel()
     {
     }
+
+    public function webhook()
+    {
+        // This is your Stripe CLI webhook secret for testing your endpoint locally.
+        $endpoint_secret = env('STRIPE_WEBHOOK_SECRET');
+
+        $payload = @file_get_contents('php://input');
+        $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+        $event = null;
+
+        try {
+            $event = \Stripe\Webhook::constructEvent(
+                $payload,
+                $sig_header,
+                $endpoint_secret
+            );
+        } catch (\UnexpectedValueException $e) {
+            // Invalid payload
+            return response('', 400);
+        } catch (\Stripe\Exception\SignatureVerificationException $e) {
+            // Invalid signature
+            return response('', 400);
+        }
+
+        // Handle the event
+        switch ($event->type) {
+            case 'checkout.session.completed':
+                $session = $event->data->object;
+
+                $order = Order::where('session_id', $session->id)->first();
+                if ($order && $order->status === 'unpaid') {
+                    $order->status = 'paid';
+                    $order->save();
+                    // Send email to customer
+                }
+
+                // ... handle other event types
+            default:
+                echo 'Received unknown event type ' . $event->type;
+        }
+
+        return response('');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', [ProductController::class, 'index']);
 Route::post('/checkout', [ProductController::class, 'checkout'])->name('checkout');
 Route::get('/success', [ProductController::class, 'success'])->name('checkout.success');
 Route::get('/cancel', [ProductController::class, 'cancel'])->name('checkout.cancel');
+Route::post('/webhook', [ProductController::class, 'webhook'])->name('checkout.webhook');
